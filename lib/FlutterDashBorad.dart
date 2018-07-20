7f4c7d13d454e951b0415650fbcbc3ecf641ba81
Akshay Sood <akshay.sood@debutinfotech.com>
2018-07-20 15:59:42 +0530

    Dashboard in flutter design

--- lib/FlutterDashBorad.dart
+++ lib/FlutterDashBorad.dart
@@ -1,93 +1,15 @@
 import 'package:flutter/material.dart';
 
 
-
-//void main() => runApp(ListViewLoadMore(items: List<String>.generate(10000, (i) => "Item $i")));
-
-//void main()=>runApp(CustomInfiniteListView(
-//  items: List<ListItem>.generate(
-//  1000,
-//      (i) => i % 6 == 0
-//      ? HeadingItem("Heading $i")
-//      : MessageItem("Sender $i", "Message body $i"),
-//),
-//));
-
-//void main()=>runApp(SwipeToDelete(items: List<String>.generate(20, (i) => "Item ${i + 1}")));
-
-//navigate from one screen to another
-//void main()=>runApp(MaterialApp(
-//  title: 'Navigation Basics',
-//  home: FirstScreen(),
-//));
-
-//send data from one screen to another screen
-//void main() {
-//  runApp(MaterialApp(
-//    title: 'Passing Data',
-//    home: TodosScreen(
-//      todos: List.generate(
-//        20,
-//            (i) => Todo(
-//          'Todo $i',
-//          'A description of what needs to be done for Todo $i',
-//        ),
-//      ),
-//    ),
-//  ));
-//}
-
-//navigate to screen and get result from
-//void main() {
-//  runApp(MaterialApp(
-//    title: 'Returning Data',
-//    home: HomeScreen(),
-//  ));
-//}
-
-//route to screen example
-//void main() {
-//  runApp(MaterialApp(
-//    title: 'Named Routes Demo',
-//    // Start the app with the "/" named route. In our case, the app will start
-//    // on the FirstScreen Widget
-//    initialRoute: '/',
-//    routes: {
-//      // When we navigate to the "/" route, build the FirstScreen Widget
-//      '/': (context) => FirstScreen(),
-//      // When we navigate to the "/second" route, build the SecondScreen Widget
-//      '/second': (context) => SecondScreen(),
-//    },
-//  ));
-//}
-
-//widget animation while navigating
-//void main()=>runApp(MaterialApp(
-//title: 'Transition Demo',
-//home: MainScreen(),
-//));
-
-//animation fade in fade out
-//void main()=>runApp(new AnimFadeInOut());
-//fetch data
-//fetch data from server
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
@@ -105,24 +27,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-       _counter++;
-    });
-  }
-
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-
     return new Scaffold(
       appBar: new AppBar(
         backgroundColor: Colors.red,
@@ -153,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
               color: const Color(0xFFFFFFFF),
 
               //first row
-              child:new Row(
+              child: new Row(
                 children: <Widget>[
                   //my profile
                   new Expanded(
@@ -730,8 +637,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
         ],
       ),
-
-
 
 
     );
